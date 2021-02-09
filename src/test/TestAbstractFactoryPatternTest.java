eac7379a2ad0ecacf95f5a2a36b5d8a6df0406f5
Samuel Moreno <sammo@LAPTOP-URVBJHIT>
2021-02-09 14:32:38 +0100

    Clase de Test

--- src/test/TestAbstractFactoryPatternTest.java
+++ src/test/TestAbstractFactoryPatternTest.java
@@ -7,9 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import static org.mockito.Mockito.mockitoSession;
 
 import java.util.logging.Logger;
 
@@ -17,33 +15,21 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-
-
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 
 
 
 /**
  * @author Samuel Moreno
- * Clase para realizar los test a la clase TestAbstractFactoryPattern
+ * Clase para realizar los test a la clase TestAbstractFactoryPattern. Se deben IMPORTAR LOS MOCKS...
  */
-@ExtendWith(MockitoExtension.class)
+
 class TestAbstractFactoryPatternTest {
 	
-	@Mock(serializable = true)
-	private static AbstractParserFactory Abstract1= Mockito.mock(AbstractParserFactory.class);
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+
+		@Test
+		void test() {
+			fail("Not yet implemented");
+		}
+
 
 }
